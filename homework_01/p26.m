310bd8fd212bb218be3e0ae42daffabcc8595065
James Folberth <jamesfolberth@gmail.com>
2014-09-25 18:59:14 -0600

    fixed axes scaling for p26.m

--- homework_01/p26.m
+++ homework_01/p26.m
@@ -12,25 +12,13 @@ function [] = plot_eigs(savefile,titlestring='',figurefilename='')
    load(savefile);
 
    for i=1:numel(n)
-      %plot(real(evals(1:n(i),:,i)(:)/sqrt(n(i))),...
-      %   imag(evals(1:n(i),:,i)(:)/sqrt(n(i))),...
-      %   'o','markerfacecolor',[0 0 1],'markersize',4);
-     
-      %hist3([real(evals(1:n(i),:,i)(:)/sqrt(n(i))),...
-      %   imag(evals(1:n(i),:,i)(:)/sqrt(n(i)))], [50 50]);
       [values, centers] = hist3([real(evals(1:n(i),:,i)(:)/sqrt(n(i))),...
          imag(evals(1:n(i),:,i)(:)/sqrt(n(i)))], [75 75]);
-      
       volume = trapz(transpose(centers{1}),...
          trapz(ones([size(values,1) 1])*centers{2},values,2),1);
-      imagesc(values/volume);
+      imagesc(centers{1},centers{2},values/volume);
       colorbar();
       axis('equal');
-
-      %plot3(real(evals(1:n(i),:,i)(:)/sqrt(n(i))),...
-      %   imag(evals(1:n(i),:,i)(:)/sqrt(n(i))),...
-      %   abs(evals(1:n(i),:,i)(:)/sqrt(n(i))),...
-      %   'o','markerfacecolor',[0 0 1],'markersize',4);
 
       xlabel('$\Re(\lambda_i/\sqrt{n})$','interpreter','tex');
       ylabel('$\Im(\lambda_i/\sqrt{n})$','interpreter','tex');
