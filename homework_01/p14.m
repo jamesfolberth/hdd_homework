43097b4ad18a166013f1a40c3f6518a65177c279
James Folberth <jamesfolberth@gmail.com>
2014-09-25 18:41:07 -0600

    added empirical computation of slab parameter

--- homework_01/p14.m
+++ homework_01/p14.m
@@ -1,12 +1,27 @@
 %GNU Octave
 function [] = p14()
+   N = 10000;
    n = (2:20).^2;
    eps = sqrt(2*log(200)./n);
+ 
+   eps_empirical = zeros([numel(n) 1]);
+   for i=1:numel(n)
+      fprintf(1,'i=%d\n',i); fflush(1);
+      x1 = sample_sphere_proj(n(i),N,1);
+      x1_sort = sort(x1);
+      %eps_empirical(i) = (abs(x1_sort(1+floor(0.005*N))) +...
+      %   abs(x1_sort(end-ceil(0.005*N))))/2;
+      eps_empirical(i) = max(abs(x1_sort(1+floor(0.005*N))),...
+         abs(x1_sort(end-ceil(0.005*N))));
+ 
+   end
 
-   plot(n,eps);
+
+
+   plot(n,eps,n,eps_empirical,'o','markerfacecolor',[0 0 1],'markersize',4);
    xlabel('Dimension $n$','interpreter','tex');
    ylabel('Slab parameter $\epsilon(n)$','interpreter','tex');
-   title('Slab parameter $\epsilon(n)$ as a function of dimension $n$',...
+   title('Slab parameter $\epsilon(n)$ as a function of dimension $n$ with emipirical data',...
       'interpreter','tex');
    print('figures/p14.tikz','-dtikz','-S640,480');
 
